4a110ce8f710f97a3472917d9bb26c4cd8197f0a
Jean-Denis <jean-denis.marie-sainte@epitech.eu>
2019-05-19 20:48:40 +0200

    Update Actions
    Still in the stack => add Feed to serv

--- src/main/java/com/jd/rss/module/Actions/Feed.java
+++ src/main/java/com/jd/rss/module/Actions/Feed.java
@@ -1,11 +1,56 @@
 package com.jd.rss.module.Actions;
 
 import com.jd.rss.module.network.network;
+import com.jd.rss.module.Feed.SendFeed;
 
 import org.springframework.http.HttpStatus;
 import com.google.gson.Gson;
 
 public class Feed
 {
+    private network client = new network("");
 
+    public String Feeds()
+    {
+        String  rez;
+
+        rez = client.get("/feeds");
+        if (rez != null)
+        {
+            System.out.println("Request Get Feeds : OK");
+            return rez;
+        }
+        System.out.println("Request Get Feeds : Failed");
+        return "KO";
+    }
+
+    public String getSecificFeed(String feed_id)
+    {
+        String rez;
+
+        rez = client.get("/feeds/" + feed_id);
+        if (rez != null)
+        {
+            System.out.println("Request Get Specific Feeds : OK");
+            return rez;
+        }
+        System.out.println("Request Get Specific Feeds : Failed");
+        return "KO";
+    }
+
+    public void addNewFeed(final String feed_id)
+    {
+
+    }
+
+    public void deleteFeed(final String feed_id)
+    {
+        client.delete("/feeds/" + feed_id);
+        if (client.getStatus() == HttpStatus.NO_CONTENT)
+        {
+            System.out.println("Feed " + feed_id + " has been deleted");
+        }
+        else
+            System.out.println("Feed " + feed_id + " don't exist");
+    }
 }
--- src/main/java/com/jd/rss/module/Actions/Users.java
+++ src/main/java/com/jd/rss/module/Actions/Users.java
@@ -1,5 +1,51 @@
 package com.jd.rss.module.Actions;
 
+import com.google.gson.Gson;
+import com.jd.rss.module.user.User_Fav;
+import com.jd.rss.module.network.network;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Arrays;
+
 public class Users
 {
+    private network client = new network("");
+
+    public String me()
+    {
+        String  rez;
+
+        rez = client.post("/users/me", "");
+        if (rez != null)
+        {
+            System.out.println("Request OK for Profle");
+            return rez;
+        }
+        System.out.println("Request Failed");
+        return "KO";
+    }
+
+    public String favFeeds_update(String feed_id)
+    {
+        Gson objson = new Gson();
+        User_Fav    fav = new User_Fav();
+        String[]  parsed = feed_id.split("\\s+|,\\s*");
+        List<String>    tmp = Arrays.asList(parsed);
+        String  json;
+
+        fav.setPreferredFeeds(tmp);
+        json = objson.toJson(fav);
+        client.put("/users/me/feeds", json);
+        if (client.getStatus() == HttpStatus.NO_CONTENT)
+        {
+            System.out.println("Update Fav List : OK");
+            return "OK";
+        }
+        System.out.println("Update Fav : Request Failed");
+        return "KO";
+    }
 }
+
+
+//TODO Ajout méthode conservation token
